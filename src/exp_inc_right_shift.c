7c6cc7210b781ea029a5690fd2f575c0f0852a1c
SatoYuki8 <sato.yuki@arc.cs.okayama-u.ac.jp>
2017-08-16 10:55:24 +0900

    commit

---
+++ src/exp_inc_right_shift.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int exp_inc_right_shift(int exp, int frac){
+  int i;
+  FILE *fp;
+  char *file = "exp_inc_right_shift.sfl";
+  
+  if ((fp = fopen(file, "w")) == NULL){
+    fprintf(stderr, "File open error\n");
+    return 0;
+  }
+
+  fprintf(fp, "circuit exp_inc_right_shift{\n");
+  fprintf(fp, "input exp<%d>;\n", exp);
+  fprintf(fp, "input frac<%d>;\n", frac);
+  fprintf(fp, "output inc_exp<%d>;\n", exp);
+  fprintf(fp, "output shifted_frac<%d>;\n", frac);
+  fprintf(fp, "instrin do;\n\n");
+
+  fprintf(fp, "innstruct do par{\n");
+  fprintf(fp, "inc_exp = exp + 0b");
+
+  for (i=0; i<exp; i++){
+    if (i == exp -1) fputs("1", fp);
+    else fputs("0", fp);
+  }
+
+  fputs(";\n", fp);
+  fputs("shifted_frac = frac >> 0b1;\n", fp);
+
+  fputs("}\n", fp);    // instruct do par {
+  fputs("}\n", fp);    // circuit exp_inc_right_shift{
+  
+  fclose(fp);
+  
+  return 0;
+}
--- src/generator.c
+++ src/generator.c
@@ -8,6 +8,8 @@ int generator(int argc, char **argv){
   exp_comp(atoi(argv[1]));
 
   sub_shift(atoi(argv[1]), atoi(argv[2]));
+
+  exp_inc_right_shift(atoi(argv[1]), atoi(argv[2]));
   
   return 0;
 }
